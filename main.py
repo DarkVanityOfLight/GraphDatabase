3f8432c1740a563fee39074cded821d74c1013bd
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2024-07-01 13:19:58 +0200

    chore: Do some type hinting

--- main.py
+++ main.py
@@ -1,30 +1,83 @@
 #!/bin/env python3
 
+from typing import Optional, Any
 import json
 import z3
 
+class NodeAttributes:
+    def __init__(self):
+        self.alphabet : dict[str, z3.Real | z3.String]= {}
+        self.attribute_map : dict[int, dict[str, str | int]]= {}
+
+    def add_variable(self, var_name, value):
+        if isinstance(value, (int, float)):
+            self.alphabet[var_name] = z3.Real(var_name)
+        elif isinstance(value, str):
+            self.alphabet[var_name] = z3.String(var_name)
+        else:
+            raise ValueError("Unsupported attribute type")
+
+    def get_variable(self, var_name: str):
+        return self.alphabet.get(var_name, None)
+    
+    def __str__(self):
+        output = "Node Attributes:\n"
+        for var_name, value in self.attribute_map.items():
+            output += f"{var_name}: {value}\n"
+        return output
+    
+
+class AutomatonTransition:
+    def __init__(self, from_state : int, to_state: int, formula):
+        self.from_state : int = from_state
+        self.to_state : int = to_state
+        self.formula = formula
+
+    def __str__(self):
+        return f"From: {self.from_state}, To: {self.to_state}, Formula: {self.formula}"
+
+
+class Automaton:
+    def __init__(self):
+        self.initial_state : int = None
+        self.transitions = []
+        self.final_states = set()
+
+    def __str__(self):
+        transitions_str = "\n".join(str(transition) for transition in self.transitions)
+        return f"Initial State: {self.initial_state}, Transitions:\n{transitions_str}, Final States: {self.final_states}"
+
+    def transitions_from(self, state: int) -> filter[int]:
+        return filter(lambda x: x.from_state == state,self.transitions)
+
+    def transitions_to(self, state : int) -> filter[int]:
+        return filter(lambda x: x.to_state == state, self.transitions)
+
+    def transitions_from_to(self, from_state : int, to_state : int) -> filter[int]:
+        return filter(lambda x: x.from_state == from_state and x.to_state == to_state, self.transitions)
+
 class Graph:
     def __init__(self):
-        self.nodes = set()
-        self.adjacency_map = {}
+        self.nodes : set[int] = set()
+        self.adjacency_map : dict[int, list[int]]= {}
 
-    def add_node(self, node):
+    def add_node(self, node: int):
         self.nodes.add(node)
         if node not in self.adjacency_map:
             self.adjacency_map[node] = []
 
-    def add_edge(self, from_node, to_node):
+    def add_edge(self, from_node: int, to_node: int):
         self.add_node(from_node)
         self.add_node(to_node)
         self.adjacency_map[from_node].append(to_node)
 
-    def __str__(self):
+    def __str__(self) -> str:
         result = "Graph:\n"
         for node in sorted(self.nodes):
             result += f"{node}: {', '.join(map(str, self.adjacency_map.get(node, [])))}\n"
         return result
 
-    def find_path(self, source, target):
+    def find_path(self, source:int, target:int) -> Optional[list[int]]:
         # This is retarded as set, but we can't be sure about the node numbering
         visited = set()
         stack = [(source, [source])]
@@ -46,51 +99,9 @@ class Graph:
 
         return None
 
-        
-
-class AutomatonTransition:
-    def __init__(self, from_state, to_state, formula):
-        self.from_state = from_state
-        self.to_state = to_state
-        self.formula = formula
-
-    def __str__(self):
-        return f"From: {self.from_state}, To: {self.to_state}, Formula: {self.formula}"
-
-class NodeAttributes:
-    def __init__(self):
-        self.alphabet = {}
-        self.attribute_map = {}
-
-    def add_variable(self, var_name, value):
-        if isinstance(value, (int, float)):
-            self.alphabet[var_name] = z3.Real(var_name)
-        elif isinstance(value, str):
-            self.alphabet[var_name] = z3.String(var_name)
-        else:
-            raise ValueError("Unsupported attribute type")
 
 
-    def get_variable(self, var_name):
-        return self.alphabet.get(var_name, None)
-    
-    def __str__(self):
-        output = "Node Attributes:\n"
-        for var_name, value in self.attribute_map.items():
-            output += f"{var_name}: {value}\n"
-        return output
-
-class Automaton:
-    def __init__(self):
-        self.initial_state = None
-        self.transitions = []
-        self.final_states = set()
-    def __str__(self):
-        transitions_str = "\n".join(str(transition) for transition in self.transitions)
-        return f"Initial State: {self.initial_state}, Transitions:\n{transitions_str}, Final States: {self.final_states}"
-
-
-def create_global_var(var_name, type):
+def create_global_var(var_name: str, type) -> z3.Real | z3.String:
         if type == "Real":
             return z3.Real(var_name)
         elif type == "String":
@@ -105,7 +116,7 @@ def merge_dicts(dict1, dict2):
 
     return {**dict1, **dict2}
 
-def parse_json_file(file_path):
+def parse_json_file(file_path: str) -> tuple[Graph, NodeAttributes, Automaton, dict[str, str | int]]:
     with open(file_path, 'r') as file:
         json_data = json.load(file)
 
@@ -126,9 +137,10 @@ def parse_json_file(file_path):
             attributes.add_variable(attr_name, attr_value)
 
     for vertex, attr in json_data["Attributes"].items():
-        attr_tuple = tuple(attr.values())
-        attribute_map[vertex] = attr_tuple
+        attribute_map[vertex] = attr
+
     attributes.attribute_map = attribute_map
+
     # Parse Automaton
     automaton.initial_state = json_data["Automaton"]["Initial State"]
     automaton.final_states = set(json_data["Automaton"]["Final States"])
@@ -158,28 +170,32 @@ if __name__ == '__main__':
     print("Formula: ", parsed_automaton.transitions[0].formula)
     print("Global Vars", global_vars)
 
-    all_variables = merge_dicts(parsed_attributes.alphabet, global_vars)
-    # Parse smt2 string with declared vars; returns vector of assertions, in our case always 1
-    test0 = z3.parse_smt2_string(parsed_automaton.transitions[0].formula, decls=all_variables)[0]
-    solver.add(test0)
-    print("test0: ", test0)
-    solver.check()
-    print("model 1:",solver.model())
-    test = z3.parse_smt2_string(parsed_automaton.transitions[1].formula, decls=all_variables)[0]
-    print("test:", test)
-    solver.add(test)
-    # Check model
-    solver.check()
-    print("model 2: ", solver.model())
+    all_variables : dict[str, str | int] = merge_dicts(parsed_attributes.alphabet, global_vars)
 
-    # Replace age by value 2
-    test4 = (parsed_attributes.alphabet['age'])
-    test5 = (global_vars['p1'])
-    # test0[0] is the first assert in the z3 ast vector
-    expr2 = z3.substitute(test0, (test4, z3.RealVal(2.0)))
-    print("Substitute age by 2: ", expr2)
+    # # Parse smt2 string with declared vars; returns vector of assertions, in our case always 1
+    # test0 = z3.parse_smt2_string(parsed_automaton.transitions[0].formula, decls=all_variables)[0]
+    # solver.add(test0)
+    # print("test0: ", test0)
+    # solver.check()
+    # print("model 1:",solver.model())
+
+
+    # test = z3.parse_smt2_string(parsed_automaton.transitions[1].formula, decls=all_variables)[0]
+    # print("test:", test)
+    # solver.add(test)
+    # # Check model
+    # solver.check()
+    # print("model 2: ", solver.model())
+
+    # # Replace age by value 2
+    # test4 = (parsed_attributes.alphabet['age'])
+    # test5 = (global_vars['p1'])
+    # # test0[0] is the first assert in the z3 ast vector
+    # expr2 = z3.substitute(test0, (test4, z3.RealVal(2.0)))
+    # print("Substitute age by 2: ", expr2)
 
     print("==========================================================")
 
-    print(parsed_graph.find_path(1, 3))
+    #print(parsed_graph.find_path(1, 3))
 
+    parsed_graph.find_query_path(1, 3, parsed_automaton, parsed_attributes, all_variables)
