51664da4995702df812bb3458eed5fa959e4ef2b
David Klaaßen <david.klaassen@web.de>
2024-07-16 16:03:12 +0200

    chore: generalize tests

--- test.py
+++ test.py
@@ -1,14 +1,31 @@
-from util import *
 import unittest
-file_path1 = 'example1.json'  # Path to your JSON file
-graph1, attributes1, automaton1, global_vars1 = parse_json_file(file_path1)
-file_path2 = 'example2.json'  # Path to your JSON file
-graph2, attributes2, automaton2, global_vars2 = parse_json_file(file_path2)
-file_path3 = 'example3.json'  # Path to your JSON file
-graph3, attributes3, automaton3, global_vars3 = parse_json_file(file_path3)
-file_path4 = 'example4.json'  # Path to your JSON file
-graph4, attributes4, automaton4, global_vars4 = parse_json_file(file_path4)
 
+from util import *
+
+graph1, attributes1, automaton1, global_vars1 = parse_json_file('example1.json')
+graph2, attributes2, automaton2, global_vars2 = parse_json_file('example2.json')
+graph3, attributes3, automaton3, global_vars3 = parse_json_file('example3.json')
+graph4, attributes4, automaton4, global_vars4 = parse_json_file('example4.json')
+
+def test_algorithm(algorithm):
+        assert algorithm(attributes1, automaton1, graph1, global_vars1, 1, 5)
+        assert algorithm(attributes1, automaton1, graph1, global_vars1, 1, 4)
+        assert algorithm(attributes1, automaton1, graph1, global_vars1, 2, 5)
+        assert not algorithm(attributes1, automaton1, graph1, global_vars1, 2, 3)
+
+        assert algorithm(attributes2, automaton2, graph2, global_vars2, 2, 3)
+        assert not algorithm(attributes2, automaton2, graph2, global_vars2, 3, 1)
+
+        assert algorithm(attributes3, automaton3, graph3, global_vars3, 4, 6)
+        assert not algorithm(attributes3, automaton3, graph3, global_vars3, 2, 5)
+        assert not algorithm(attributes3, automaton3, graph3, global_vars3, 2, 3)
+
+        assert algorithm(attributes4, automaton4, graph4, global_vars4, 1, 13)
+        assert algorithm(attributes4, automaton4, graph4, global_vars4, 1, 10)
+        assert algorithm(attributes4, automaton4, graph4, global_vars4, 3, 10)
+        assert not algorithm(attributes4, automaton4, graph4, global_vars4, 1, 7)
+        assert not algorithm(attributes4, automaton4, graph4, global_vars4, 3, 7)
+        assert not algorithm(attributes4, automaton4, graph4, global_vars4, 9, 7)
 
 class Test(unittest.TestCase):
 
@@ -20,41 +37,7 @@ class Test(unittest.TestCase):
         assert not graph3.get_paths(2, 1)
 
     def test_naive_algorithm(self):
-        assert query_with_naive_algorithm(attributes1, automaton1, graph1, global_vars1, 1, 5)
-        assert query_with_naive_algorithm(attributes1, automaton1, graph1, global_vars1, 1, 4)
-        assert query_with_naive_algorithm(attributes1, automaton1, graph1, global_vars1, 2, 5)
-        assert not query_with_naive_algorithm(attributes1, automaton1, graph1, global_vars1, 2, 3)
-
-        assert not query_with_naive_algorithm(attributes2, automaton2, graph2, global_vars2, 3, 1)
-        assert query_with_naive_algorithm(attributes2, automaton2, graph2, global_vars2, 2, 3)
-
-        assert query_with_naive_algorithm(attributes3, automaton3, graph3, global_vars3, 4, 6)
-        assert not query_with_naive_algorithm(attributes3, automaton3, graph3, global_vars3, 2, 5)
-        assert not query_with_naive_algorithm(attributes3, automaton3, graph3, global_vars3, 2, 3)
-
-        assert query_with_naive_algorithm(attributes4, automaton4, graph4, global_vars4, 1, 13)
-        assert query_with_naive_algorithm(attributes4, automaton4, graph4, global_vars4, 1, 10)
-        assert query_with_naive_algorithm(attributes4, automaton4, graph4, global_vars4, 3, 10)
-        assert not query_with_naive_algorithm(attributes4, automaton4, graph4, global_vars4, 1, 7)
-        assert not query_with_naive_algorithm(attributes4, automaton4, graph4, global_vars4, 3, 7)
-        assert not query_with_naive_algorithm(attributes4, automaton4, graph4, global_vars4, 9, 7)
+        test_algorithm(query_with_naive_algorithm)
 
     def test_macro_state_algorithm(self):
-        assert query_with_macro_state(attributes1, automaton1, graph1, global_vars1, 1, 5)
-        assert query_with_macro_state(attributes1, automaton1, graph1, global_vars1, 1, 4)
-        assert not query_with_macro_state(attributes1, automaton1, graph1, global_vars1, 1, 2)
-        assert not query_with_macro_state(attributes1, automaton1, graph1, global_vars1, 2, 3)
-
-        assert not query_with_macro_state(attributes2, automaton2, graph2, global_vars2, 3, 1)
-        assert query_with_macro_state(attributes2, automaton2, graph2, global_vars2, 2, 3)
-
-        assert query_with_macro_state(attributes3, automaton3, graph3, global_vars3, 4, 6)
-        assert not query_with_macro_state(attributes3, automaton3, graph3, global_vars3, 2, 5)
-        assert not query_with_macro_state(attributes3, automaton3, graph3, global_vars3, 2, 3)
-
-        assert query_with_macro_state(attributes4, automaton4, graph4, global_vars4, 1, 13)
-        assert query_with_macro_state(attributes4, automaton4, graph4, global_vars4, 1, 10)
-        assert query_with_macro_state(attributes4, automaton4, graph4, global_vars4, 3, 10)
-        assert not query_with_macro_state(attributes4, automaton4, graph4, global_vars4, 1, 7)
-        assert not query_with_macro_state(attributes4, automaton4, graph4, global_vars4, 3, 7)
-        assert not query_with_macro_state(attributes4, automaton4, graph4, global_vars4, 9, 7)
+        test_algorithm(query_with_macro_state)
