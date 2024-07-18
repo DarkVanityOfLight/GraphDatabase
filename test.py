87eecab2703fec472a823307e1c6c70d2aef6961
DarkVanityOfLight <darkvanity.oflight@gmail.com>
2024-07-18 10:04:18 +0200

    chore: Fix back tests

--- test.py
+++ test.py
@@ -7,25 +7,27 @@ graph2, attributes2, automaton2, global_vars2 = parse_json_file('example2.json')
 graph3, attributes3, automaton3, global_vars3 = parse_json_file('example3.json')
 graph4, attributes4, automaton4, global_vars4 = parse_json_file('example4.json')
 
+
 def test_algorithm(algorithm):
-        assert algorithm(attributes1, automaton1, graph1, global_vars1, 1, 5)
-        assert algorithm(attributes1, automaton1, graph1, global_vars1, 1, 4)
-        assert algorithm(attributes1, automaton1, graph1, global_vars1, 2, 5)
-        assert not algorithm(attributes1, automaton1, graph1, global_vars1, 2, 3)
+    assert algorithm(attributes1, automaton1, graph1, global_vars1, 1, 5)
+    assert algorithm(attributes1, automaton1, graph1, global_vars1, 1, 4)
+    assert algorithm(attributes1, automaton1, graph1, global_vars1, 2, 5)
+    assert not algorithm(attributes1, automaton1, graph1, global_vars1, 2, 3)
 
-        assert algorithm(attributes2, automaton2, graph2, global_vars2, 2, 3)
-        assert not algorithm(attributes2, automaton2, graph2, global_vars2, 3, 1)
+    assert algorithm(attributes2, automaton2, graph2, global_vars2, 2, 3)
+    assert not algorithm(attributes2, automaton2, graph2, global_vars2, 3, 1)
 
-        assert algorithm(attributes3, automaton3, graph3, global_vars3, 4, 6)
-        assert not algorithm(attributes3, automaton3, graph3, global_vars3, 2, 5)
-        assert not algorithm(attributes3, automaton3, graph3, global_vars3, 2, 3)
+    assert algorithm(attributes3, automaton3, graph3, global_vars3, 4, 6)
+    assert not algorithm(attributes3, automaton3, graph3, global_vars3, 2, 5)
+    assert not algorithm(attributes3, automaton3, graph3, global_vars3, 2, 3)
 
-        assert algorithm(attributes4, automaton4, graph4, global_vars4, 1, 13)
-        assert algorithm(attributes4, automaton4, graph4, global_vars4, 1, 10)
-        assert algorithm(attributes4, automaton4, graph4, global_vars4, 3, 10)
-        assert not algorithm(attributes4, automaton4, graph4, global_vars4, 1, 7)
-        assert not algorithm(attributes4, automaton4, graph4, global_vars4, 3, 7)
-        assert not algorithm(attributes4, automaton4, graph4, global_vars4, 9, 7)
+    assert algorithm(attributes4, automaton4, graph4, global_vars4, 1, 13)
+    assert algorithm(attributes4, automaton4, graph4, global_vars4, 1, 10)
+    assert algorithm(attributes4, automaton4, graph4, global_vars4, 3, 10)
+    assert not algorithm(attributes4, automaton4, graph4, global_vars4, 1, 7)
+    assert not algorithm(attributes4, automaton4, graph4, global_vars4, 3, 7)
+    assert not algorithm(attributes4, automaton4, graph4, global_vars4, 9, 7)
+
 
 class Test(unittest.TestCase):
 
@@ -39,11 +41,8 @@ class Test(unittest.TestCase):
     def test_naive_algorithm(self):
         test_algorithm(query_with_naive_algorithm)
 
-    # def test_macro_state_algorithm(self):
-    #     test_algorithm(query_with_macro_state)
-
     def test_macro_state_algorithm(self):
-        test_algorithm(dk_query_with_macro_state)
+        test_algorithm(query_with_macro_state)
 
     def test_minimize_constraints(self):
         vars = global_vars3
